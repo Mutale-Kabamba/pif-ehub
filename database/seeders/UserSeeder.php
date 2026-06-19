c3e7b1dd0bf081d9b383daecd93426b5fee607ff
Mutale-Kabamba <190031@zcuniversity.edu.zm>
2026-06-19 23:52:16 +0200

    user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class UserSeeder extends Seeder
@@ -13,69 +12,75 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // Super User
-        User::create([
-            'name' => 'Super User',
-            'email' => 'super@pif.zm',
-            'password' => Hash::make('PIF_Admin_2026'),
-            'role' => 'super',
-            'panelist_name' => null,
-        ]);
+        // 1. Super User
+        // Note: Hash::make() omitted because your User model has 'password' => 'hashed' in $casts
+        User::updateOrCreate(
+            ['email' => 'super@pif.zm'],
+            [
+                'name' => 'Super User',
+                'password' => 'PIF_Admin_2026', 
+                'role' => 'super',
+                'panelist_name' => null,
+            ]
+        );
 
-        // Panelists
+        // 2. Panelists Array
         $panelists = [
             [
                 'name' => 'Mwiinga',
-                'email' => 'rabecca@pif.zm',
-                'password' => Hash::make('PIF_Mwi_2026'),
-                'panelist_name' => 'Rabecca',
+                'email' => 'mwiinga@pif.zm',
+                'password' => 'PIF_Mwi_2026',
+                'panelist_name' => 'Mwiinga',
             ],
             [
                 'name' => 'Sarah',
                 'email' => 'sarah@pif.zm',
-                'password' => Hash::make('PIF_Sar_2026'),
+                'password' => 'PIF_Sar_2026',
                 'panelist_name' => 'Sarah',
             ],
             [
                 'name' => 'Bracious',
                 'email' => 'bracious@pif.zm',
-                'password' => Hash::make('PIF_Bra_2026'),
+                'password' => 'PIF_Bra_2026',
                 'panelist_name' => 'Bracious',
             ],
             [
                 'name' => 'Mutale',
                 'email' => 'mutale@pif.zm',
-                'password' => Hash::make('PIF_Mut_2026'),
+                'password' => 'PIF_Mut_2026',
                 'panelist_name' => 'Mutale',
             ],
             [
                 'name' => 'Blessing',
                 'email' => 'blessing@pif.zm',
-                'password' => Hash::make('PIF_Ble_2026'),
+                'password' => 'PIF_Ble_2026',
                 'panelist_name' => 'Blessing',
             ],
             [
                 'name' => 'Jacqueline',
-                'email' => 'blessing@pif.zm',
-                'password' => Hash::make('PIF_Jac_2026'),
-                'panelist_name' => 'Blessing',
+                'email' => 'jacqueline@pif.zm',
+                'password' => 'PIF_Jac_2026',
+                'panelist_name' => 'Jacqueline',
             ],
             [
                 'name' => 'Florence',
-                'email' => 'blessing@pif.zm',
-                'password' => Hash::make('PIF_Flo_2026'),
-                'panelist_name' => 'Blessing',
+                'email' => 'florence@pif.zm',
+                'password' => 'PIF_Flo_2026',
+                'panelist_name' => 'Florence',
             ],
         ];
 
+        // 3. Loop and use updateOrCreate to prevent unique constraint crashes
         foreach ($panelists as $panelist) {
-            User::create([
-                'name' => $panelist['name'],
-                'email' => $panelist['email'],
-                'password' => $panelist['password'],
-                'role' => 'panelist',
-                'panelist_name' => $panelist['panelist_name'],
-            ]);
+            User::updateOrCreate(
+                ['email' => $panelist['email']], // Check if this email exists
+                [
+                    'name' => $panelist['name'],
+                    'password' => $panelist['password'],
+                    'role' => 'panelist',
+                    'panelist_name' => $panelist['panelist_name'],
+                ]
+            );
         }
     }
 }
