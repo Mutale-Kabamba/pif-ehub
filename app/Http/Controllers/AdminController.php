e1aedaff34bd5eac6210e4c6b47e546da8a1fe78
Mutale-Kabamba <190031@zcuniversity.edu.zm>
2026-07-10 08:32:38 +0200

    Update qualitative survey questions to reflect training context

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -27,9 +27,9 @@ class AdminController extends Controller
     ];
 
     private array $qualQuestions = [
-        'qual1_why_join'       => "Why did you decide to join this project?",
-        'qual2_skills_hoped'   => "What skills or knowledge are you hoping to gain, and what kind of project would you like to build during this project?",
-        'qual3_success_criteria' => "At the end of this project, what would make you...",
+        'qual1_why_join'       => "Why did you decide to join this training?",
+        'qual2_skills_hoped'   => "What skills or knowledge are you hoping to gain during this training?",
+        'qual3_success_criteria' => "At the end of this training, what would make you...",
         'qual4_challenges'     => "What challenges do you anticipate might make it...",
     ];
 
